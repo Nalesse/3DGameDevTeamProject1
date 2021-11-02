3475bc4f5a4c5635598e0dec4a3cd9498ec3e280
Nalesse <nalesse@student.fitchburgstate.edu>
2021-11-02 14:49:41 -0400

    Basic Setup of Player and Enemy classes

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //This class holds shared attributes for the player and Enemy classes
+
+    public int health;
+}
