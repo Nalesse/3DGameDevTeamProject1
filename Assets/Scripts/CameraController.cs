5ebd7279ef5f65fa0b588c0f4e43ed808171aff1
Nalesse <nalesse@student.fitchburgstate.edu>
2021-11-13 13:27:54 -0500

    Finished Camera Scroll
    The Script might need some refactoring at a later date, but all the functionality is done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Tooltip("How much the player is allowed to move on the x axis before moving the camera")]
     [SerializeField] private float xLimit;
+
+    [Tooltip("How much to offset the camera on the x axis when it moves")]
+    [SerializeField] private float xSnapPos;
+
     [Range(0.0f, 1.0f)]
     [Tooltip("0 = more smoothness, 1 = less smoothness")]
     [SerializeField] private float cameraSmoothness;
+
     private Vector3 offset;
     private Player player;
 
@@ -23,11 +29,17 @@ public class CameraController : MonoBehaviour
         var step = cameraSmoothness / Time.deltaTime;
         var xDistance = transform.position.x + xLimit;
         var negXDistance = transform.position.x - xLimit;
-        if (player.transform.position.x >= xDistance || player.transform.position.x <= negXDistance)
+        var snapPos = new Vector3(xSnapPos, offset.y, offset.z);
+
+        if (player.transform.position.x >= xDistance)
         {
             // Moves the camera slowly to the target position by the step amount each frame until the desired position is reached.
             // This makes the camera movement smoother
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position + offset, step);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position + snapPos, step);
+        }
+        else if (player.transform.position.x <= negXDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position + Vector3.Reflect(snapPos, Vector3.left), step);
         }
     }
 }
